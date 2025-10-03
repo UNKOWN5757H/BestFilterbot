6e01d14925530586547dad22dc34ad05d6e7478d
UNKOWN5757H <65911268+UNKOWN5757H@users.noreply.github.com>
2025-10-03 11:25:30 +0530

    Update script.py

--- script.py
+++ script.py
@@ -38,9 +38,9 @@ Add me as admin in your group and start filtering :)
 """
 
 AboutTxT = """
-⭕️ Official Channel : @learningbots79</b>
-⭕️ Developers : @TroJanzHEX 
-⭕️ Upgrader : @Mo_Tech_YT
+⭕️ Official Channel : @KR_PICTURE</b>
+⭕️ Developers : @Nikhil5757h 
+⭕️ Upgrader : @KR_PICTURE 
 ⭕️ Language : Python3
 ⭕️ Library : <a href='https://docs.pyrogram.org/'>Pyrogram 2.0.102</a> 
 """
